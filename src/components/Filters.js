a3a51d8fad2c58be2688aee374d8b9dfb083781b
Fernando Villasenor <fvilla18@gmail.com>
2020-07-01 15:20:08 -0700

    Add responsive layout changes

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -20,72 +20,74 @@ const Filters = ({
 }) => {
   return (
     <div className="filters">
-      <p className="filter-by-text">Sort by:</p>
+      <div className="filter-container">
+        <p className="filter-by-text">Sort by:</p>
 
-      <div className="dropdown">
-        <div className="dropdown-select">
-          <span>{selectedSort || 'Select'}</span> <span className="dropdown-icon">&#x25BC;</span>
+        <div className="dropdown">
+          <div className="dropdown-select">
+            <span>{selectedSort || 'Select'}</span> <span className="dropdown-icon">&#x25BC;</span>
+          </div>
+
+          <div className="dropdown-options">
+            <div className="dropdown-option" onClick={() => onSortChange(SORT_NAME_ASC)}>
+              {SORT_NAME_ASC}
+            </div>
+
+            <div className="dropdown-option" onClick={() => onSortChange(SORT_NAME_DESC)}>
+              {SORT_NAME_DESC}
+            </div>
+
+            <div className="dropdown-option" onClick={() => onSortChange(SORT_POPULATION_ASC)}>
+              {SORT_POPULATION_ASC}
+            </div>
+
+            <div className="dropdown-option" onClick={() => onSortChange(SORT_POPULATION_DESC)}>
+              {SORT_POPULATION_DESC}
+            </div>
+          </div>
         </div>
+      </div>
 
-        <div className="dropdown-options">
-          <div className="dropdown-option" onClick={() => onSortChange(SORT_NAME_ASC)}>
-            {SORT_NAME_ASC}
+      <div className="filter-container">
+        <p className="filter-by-text">Filter by Subregion</p>
+
+        <div className="dropdown">
+          <div className="dropdown-select">
+            <span>{selectedRegionFilter || 'Select'}</span> <span className="dropdown-icon">&#x25BC;</span>
           </div>
 
-          <div className="dropdown-option" onClick={() => onSortChange(SORT_NAME_DESC)}>
-            {SORT_NAME_DESC}
-          </div>
-
-          <div className="dropdown-option" onClick={() => onSortChange(SORT_POPULATION_ASC)}>
-            {SORT_POPULATION_ASC}
-          </div>
-
-          <div className="dropdown-option" onClick={() => onSortChange(SORT_POPULATION_DESC)}>
-            {SORT_POPULATION_DESC}
+          <div className="dropdown-options">
+            {availableSubregions.map(region => {
+              return (
+                <div className="dropdown-option" onClick={() => onRegionFilterChange(region)} key={region}>
+                  {region}
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
 
-      <br />
+      <div className="filter-container">
+        <p className="filter-by-text">Filter by # Languages</p>
 
-      <p className="filter-by-text">Subregion</p>
-
-      <div className="dropdown">
-        <div className="dropdown-select">
-          <span>{selectedRegionFilter || 'Select'}</span> <span className="dropdown-icon">&#x25BC;</span>
-        </div>
-
-        <div className="dropdown-options">
-          {availableSubregions.map(region => {
-            return (
-              <div className="dropdown-option" onClick={() => onRegionFilterChange(region)} key={region}>
-                {region}
-              </div>
-            );
-          })}
-        </div>
-      </div>
-
-      <br />
-
-      <p className="filter-by-text">Languages</p>
-
-      <div className="dropdown">
-        <div className="dropdown-select">
-          <span>{selectedLanguagesFilter || 'Select'}</span> <span className="dropdown-icon">&#x25BC;</span>
-        </div>
-
-        <div className="dropdown-options">
-          <div className="dropdown-option" onClick={() => onLanguageFilterChange(EQUAL_COMPARE, '1')}>
-            1
+        <div className="dropdown">
+          <div className="dropdown-select">
+            <span>{selectedLanguagesFilter || 'Select'}</span> <span className="dropdown-icon">&#x25BC;</span>
           </div>
 
-          <div className="dropdown-option" onClick={() => onLanguageFilterChange(EQUAL_COMPARE, '2')}>
-            2
-          </div>
+          <div className="dropdown-options">
+            <div className="dropdown-option" onClick={() => onLanguageFilterChange(EQUAL_COMPARE, '1')}>
+              1
+            </div>
 
-          <div className="dropdown-option" onClick={() => onLanguageFilterChange(GREATER_THAN_COMPARE, '3+')}>
-            3+
+            <div className="dropdown-option" onClick={() => onLanguageFilterChange(EQUAL_COMPARE, '2')}>
+              2
+            </div>
+
+            <div className="dropdown-option" onClick={() => onLanguageFilterChange(GREATER_THAN_COMPARE, '3+')}>
+              3+
+            </div>
           </div>
         </div>
       </div>
